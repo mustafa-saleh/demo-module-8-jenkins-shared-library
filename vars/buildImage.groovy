a9c9019e66393181af6861de4ee0355591040472
mustafa abdelgader <mustafa-saleh>
2026-07-30 15:01:08 +0400

    update docker repo

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-3.0 .'
+        sh 'docker build -t mustafa199b/demo:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-3.0'
+        sh 'docker push mustafa199b/demo:jma-2.0'
     }
 }
